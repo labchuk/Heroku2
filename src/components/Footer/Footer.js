68e4ed6959165e4613302d1b19858ee50ebe4f9b
Yevheniia Lukianenko <zeffir54@gmail.com>
2021-06-11 20:19:52 +0300

    Corrected conflicts for merge

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import { Logo } from '../index';
-import style from "./Footer.module.scss";
+import "./Footer.scss";
 import phone from '../../images/icons/phone.svg';
 import email from '../../images/icons/email.svg';
 
 const Footer = () => {
     return (
-        <footer className={style.footer}>
+        <footer className="footer">
            <Logo/>
            
-           <div className={style.footerContact}>
-               <a className={style.footerWeb} href="www.exadel.com">www.exadel.com</a>
-                <div className={style.footerLink}>
+           <div className="footerContact">
+               <a className="footerWeb" href="www.exadel.com">www.exadel.com</a>
+                <div className="footerLink">
                     <img src={phone} alt="icon"/>
                     <a href="tel:+3802222222">+380 2222 222</a>
                 </div>
-                <div className={style.footerLink}>
+                <div className="footerLink">
                     <img src={email} alt="icon"/>
                     <a href="mailto: info@exadel.com">info@exadel.com</a>
                 </div>
            </div>
            
-           <div className={style.footerSocial}>
+           <div className="footerSocial">
                <a href="https://www.linkedin.com/company/exadel/" target="_blank">
                     <i className="icon-linkedin"></i>
                </a>
