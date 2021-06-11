c6978f8feeae5c24ef7cd691a7208164bcdbbffe
vlad0need <ov4lil@icloud.com>
2021-06-11 15:06:09 +0300

    Start header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,10 @@ const Header = () => {
     return (
         <div className={"header"}>
           <Logo/>
-          <div className={"gravatar"}><img src="https://zbyhoo.files.wordpress.com/2011/07/me_pixel_bright_bg.png?w=584" alt=""/></div>
+          <div className={"gravatar"}>
+              <div className={"username"}>username</div>
+              <img src="https://zbyhoo.files.wordpress.com/2011/07/me_pixel_bright_bg.png?w=584" alt=""/>
+          </div>
         </div>
     );
 };
