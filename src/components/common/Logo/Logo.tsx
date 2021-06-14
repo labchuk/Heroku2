5658caae844bfb017d38adf2c4f0fdbd85f0476b
Anna-Mariia <annamariypetrik@gmail.com>
2021-06-14 16:42:07 +0300

    AuthTS

--- src/components/common/Logo/Logo.tsx
+++ src/components/common/Logo/Logo.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import "./Logo.scss";
 
-const Logo = () => {
+const Logo: React.FC = () => {
     return (
-        <div className={"Logo"}>
-            <span className={"logo__logo"}>logo</span>
-            <span className={"logo__name"}>name company</span>
-            <span className={"logo__motto"}>
+        <div className="Logo">
+            <span className="logo__logo">logo</span>
+            <span className="logo__name">name company</span>
+            <span className="logo__motto">
                 Collected everything for you
             </span>
         </div>
