435dc07187dedde55fc6503cb75d75f10ddab4f4
vlad0need <ov4lil@icloud.com>
2021-06-15 11:47:25 +0300

    Gravatar & added logo company

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,44 +4,27 @@ import "./Header.scss";
 import Button from '@material-ui/core/Button';
 import Menu, {MenuProps} from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
-import { withStyles } from '@material-ui/core/styles';
 import {NavLink} from "react-router-dom";
+import {Hidden, makeStyles} from "@material-ui/core";
+import {styled} from "@material-ui/core";
 
-const StyledMenu = withStyles({
-    paper: {
-        border: '1px solid #d3d4d5',
+
+const useStyles = makeStyles({
+    link: {
+        backgroundColor: "#F7F9FB",
+        color: "#1877F2",
     },
-})((props: MenuProps) => (
-    <Menu
-        style={{maxWidth: "600px"}}
-        elevation={0}
-        getContentAnchorEl={null}
-        anchorOrigin={{
-            vertical: 'bottom',
-            horizontal: 'center',
-        }}
-        transformOrigin={{
-            vertical: 'top',
-            horizontal: 'center',
-        }}
-        {...props}
-    />
-));
+    menu: {
+        maxWidth: 1000,
+        marginTop: "50px",
+        padding: 0,
+    }
+});
 
-
-const StyledMenuItem = withStyles((theme) => ({
-    root: {
-        '&:focus': {
-            backgroundColor: theme.palette.primary.main,
-            '& .MuiListItemIcon-root, & .MuiListItemText-primary': {
-                color: theme.palette.common.white,
-            },
-        },
-    },
-}))(MenuItem);
 
 
   export default function Header () {
+      const classes = useStyles();
         const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
 
         const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -51,34 +34,47 @@ const StyledMenuItem = withStyles((theme) => ({
         const handleClose = () => {
             setAnchorEl(null);
         };
-
+            let admin = true;
 
 
     return (
         <div className={"header"}>
           <Logo/>
-          <div className={"gravatar"}>
+          <div className={"desktop-nav"}>
+            <NavLink to={"/main-page"} activeClassName={"activelink"} >Home</NavLink>
+            <NavLink to={"/history"} activeClassName={"activelink"} >History</NavLink>
+            <NavLink to={"/statistic"} activeClassName={"activelink"} >Statistics</NavLink>
+          </div>
 
+          <div className={"gravatar"}>
               <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick} >
                   <div className={"username"}>username</div>
-                  <img src="https://zbyhoo.files.wordpress.com/2011/07/me_pixel_bright_bg.png?w=584" alt=""/>
+
               </Button>
-              <StyledMenu
+
+              <Menu
+                  className={classes.menu}
                   id="simple-menu"
                   anchorEl={anchorEl}
                   keepMounted
                   open={Boolean(anchorEl)}
                   onClose={handleClose}
+                  MenuListProps={{
+                      disablePadding: true
+                  }}
+
               >
-                  <MenuItem onClick={handleClose}>Username</MenuItem>
-                  <MenuItem onClick={handleClose}>Home</MenuItem>
-                  <MenuItem onClick={handleClose}>History</MenuItem>
-                  <MenuItem onClick={handleClose}><NavLink to={"/login"} style={{color: "red"}}>Logout</NavLink></MenuItem>
+                  <MenuItem onClick={handleClose} className={classes.link}>Username</MenuItem>
+                  <Hidden mdUp >
+                  <MenuItem onClick={handleClose} className={classes.link}><NavLink to={"/main-page"} className={"link"}>Home</NavLink></MenuItem>
+                  <MenuItem onClick={handleClose} className={classes.link}><NavLink to={"/history"} className={"link"} >History</NavLink></MenuItem>
+                  </Hidden>
+                  <MenuItem onClick={handleClose} className={classes.link}><NavLink to={"/login"} style={{color: "#E44949"}} className={"link"} >Logout</NavLink></MenuItem>
 
-              </StyledMenu>
 
-              {/*<div className={"username"}>username</div>
-              <img src="https://zbyhoo.files.wordpress.com/2011/07/me_pixel_bright_bg.png?w=584" alt=""/>*/}
+              {admin && <> </>
+              }
+              </Menu>
           </div>
         </div>
     );
