6ff36416327e192c5b03fcfe3e8d936a3f61bd27
vlad0need <ov4lil@icloud.com>
2021-06-21 12:19:16 +0300

    update sort component

--- src/components/common/Sort/Sort.tsx
+++ src/components/common/Sort/Sort.tsx
@@ -1,19 +1,32 @@
 import "./Sort.scss"
-import React from "react";
+import React, {useState} from "react";
 import {FormControl, makeStyles, NativeSelect} from "@material-ui/core";
 import { InputLabel } from "@material-ui/core";
 import { Select } from "@material-ui/core";
 import MenuItem from '@material-ui/core/MenuItem';
+import { useEffect } from "react";
 
 
 const useStyles = makeStyles((theme) => ({
     sort: {
         width: "150px",
         background: "#fff",
+
     },
 }))
 
 const Sort = () => {
+    const [currentSort, setCurrentSort] = useState("Popular")
+    const handleChangeSort = (event: any) => {
+        setCurrentSort(event.target.value)
+
+    }
+    useEffect(() => {
+        console.log(currentSort)
+    }, [currentSort]);
+
+
+
 
 const classes = useStyles();
 
@@ -22,11 +35,16 @@ const classes = useStyles();
             <div>
                 <FormControl  >
                     <InputLabel>Sort by</InputLabel>
-                    <Select defaultValue="2" id="grouped-native-select" className={classes.sort}>
-                        <MenuItem value={1}>Last Days</MenuItem>
-                        <MenuItem value={2}>Popular</MenuItem>
-                        <MenuItem value={3}>New</MenuItem>
-                        <MenuItem value={4}>Hot Sales</MenuItem>
+                    <Select
+                            id="grouped-native-select"
+                            value={currentSort}
+                            className={classes.sort}
+                            onChange={handleChangeSort}
+                    >
+                        <MenuItem value={"Ending soon"}>Ending soon</MenuItem>
+                        <MenuItem value={"Popular"}>Popular</MenuItem>
+                        <MenuItem value={"New"}>New</MenuItem>
+                        <MenuItem value={"Hot Sales"}>Hot Sales</MenuItem>
                     </Select>
                 </FormControl>
 
