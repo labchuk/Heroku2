5c4f5c1b4525d5e795a8bcfbac6a25e366ef3c22
Volodymyr Sheremeta <volosher@gmail.com>
2021-06-28 14:02:06 +0300

    fields name create

--- src/components/admin/statistic/AdditionalDate/AdditionalDate.tsx
+++ src/components/admin/statistic/AdditionalDate/AdditionalDate.tsx
@@ -6,28 +6,33 @@ import { DataGrid, GridColDef } from '@material-ui/data-grid';
 
 
 const columns: GridColDef[] = [
-    { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'firstName', headerName: 'First name', width: 130 },
-    { field: 'lastName', headerName: 'Last name', width: 130 },
-    {
+    { field: 'brandName', headerName: 'Brand Name', width: 130 },
+    { field: 'promoName', headerName: 'Promo Name', width: 130 },
+    { field: 'category', headerName: 'Category', width: 130 },
+    { field: 'date', headerName: 'Date of Use', width: 200 },
+    { field: 'userName', headerName: 'User Name', width: 130 },
+    { field: 'department', headerName: 'Department', width: 130 },
+    { field: 'location', headerName: 'Location', width: 130 },
+    { field: 'link', headerName: 'Link', width: 130 },
+    /*{
         field: 'age',
         headerName: 'Age',
         type: 'number',
         width: 130,
-    },
+    },*/
 
 ];
 
 const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 1, location: 'Kyiv', department: 'HR', userName: 'Jon', age: 35 },
+   /* { id: 2, lastName: 'Lannister', userName: 'Cersei', age: 42 },
+    { id: 3, lastName: 'Lannister', userName: 'Jaime', age: 45 },
+    { id: 4, lastName: 'Stark', userName: 'Arya', age: 16 },
+    { id: 5, lastName: 'Targaryen', userName: 'Daenerys', age: null },
+    { id: 6, lastName: 'Melisandre', userName: 'Voland', age: 150 },
+    { id: 7, lastName: 'Clifford', userName: 'Ferrara', age: 44 },
+    { id: 8, lastName: 'Frances', userName: 'Rossini', age: 36 },
+    { id: 9, lastName: 'Roxie', userName: 'Harvey', age: 65 },*/
 ];
 
 
