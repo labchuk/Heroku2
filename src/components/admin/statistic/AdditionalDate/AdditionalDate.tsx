f4ba912edbab1536c97625ffda0da8d931156fea
Volodymyr Sheremeta <volosher@gmail.com>
2021-06-29 15:40:32 +0300

    value brand changed on Vendor

--- src/components/admin/statistic/AdditionalDate/AdditionalDate.tsx
+++ src/components/admin/statistic/AdditionalDate/AdditionalDate.tsx
@@ -6,7 +6,7 @@ import { DataGrid, GridColDef } from '@material-ui/data-grid';
 
 
 const columns: GridColDef[] = [
-    { field: 'brandName', headerName: 'Brand Name', width: 130 },
+    { field: 'vendorName', headerName: 'Vendor Name', width: 130 },
     { field: 'promoName', headerName: 'Promo Name', width: 130 },
     { field: 'category', headerName: 'Category', width: 130 },
     { field: 'date', headerName: 'Date of Use', width: 170 },
@@ -19,18 +19,18 @@ const columns: GridColDef[] = [
 ];
 
 const rows = [
-    { id: 1, brandName: 'Faberlic', promoName: 'Chanel #5', category: 'Beauty', date: '12.07.2021', location: 'Kyiv', department: 'HR', userName: 'Jon', link: 'www.faberlic.com' },
-    { id: 2, brandName: 'Addidas', promoName: 'Boll of Hell', category: 'Sport', date: '18.06.2021', location: 'Minsk', department: 'FrontEnd', userName: 'Bill', link: 'www.addidas.com' },
-    { id: 3, brandName: 'Roshen', promoName: 'Cockies for 50', category: 'Food', date: '29.07.2021', location: 'Lviv', department: 'BackEnd', userName: 'Smith', link: 'www.roshen.com' },
-    { id: 4, brandName: 'BMW', promoName: 'Pedals', category: 'Cars', date: '11.07.2021', location: 'Rome', department: 'PM', userName: 'Gretta', link: 'www.bmw.com' },
-    { id: 5, brandName: 'Danone', promoName: 'No way eat our yogurts', category: 'Food', date: '02.07.2021', location: 'Konotop', department: 'QI', userName: 'Paul', link: 'www.dannone.com' },
-    { id: 6, brandName: 'Winchester', promoName: 'Fast bullets ', category: 'Guns', date: '01.07.2021', location: 'London', department: 'FrontEnd', userName: 'Petya', link: 'www.guns.com' },
-    { id: 7, brandName: 'Faberlic', promoName: 'Chanel #5', category: 'Beauty', date: '12.07.2021', location: 'Kyiv', department: 'HR', userName: 'Jon', link: 'www.faberlic.com' },
-    { id: 8, brandName: 'Addidas', promoName: 'Boll of Hell', category: 'Sport', date: '18.06.2021', location: 'Minsk', department: 'FrontEnd', userName: 'Bill', link: 'www.addidas.com' },
-    { id: 9, brandName: 'Roshen', promoName: 'Cockies for 50', category: 'Food', date: '29.07.2021', location: 'Lviv', department: 'BackEnd', userName: 'Smith', link: 'www.roshen.com' },
-    { id: 10, brandName: 'BMW', promoName: 'Pedals', category: 'Cars', date: '11.07.2021', location: 'Rome', department: 'PM', userName: 'Gretta', link: 'www.bmw.com' },
-    { id: 11, brandName: 'Danone', promoName: 'No way eat our yogurts', category: 'Food', date: '02.07.2021', location: 'Konotop', department: 'QI', userName: 'Paul', link: 'www.dannone.com' },
-    { id: 12, brandName: 'Winchester', promoName: 'Fast bullets ', category: 'Guns', date: '01.07.2021', location: 'London', department: 'FrontEnd', userName: 'Petya', link: 'www.guns.com' },
+    { id: 1, vendorName: 'Faberlic', promoName: 'Chanel #5', category: 'Beauty', date: '12.07.2021', location: 'Kyiv', department: 'HR', userName: 'Jon', link: 'www.faberlic.com' },
+    { id: 2, vendorName: 'Addidas', promoName: 'Boll of Hell', category: 'Sport', date: '18.06.2021', location: 'Minsk', department: 'FrontEnd', userName: 'Bill', link: 'www.addidas.com' },
+    { id: 3, vendorName: 'Roshen', promoName: 'Cockies for 50', category: 'Food', date: '29.07.2021', location: 'Lviv', department: 'BackEnd', userName: 'Smith', link: 'www.roshen.com' },
+    { id: 4, vendorName: 'BMW', promoName: 'Pedals', category: 'Cars', date: '11.07.2021', location: 'Rome', department: 'PM', userName: 'Gretta', link: 'www.bmw.com' },
+    { id: 5, vendorName: 'Danone', promoName: 'No way eat our yogurts', category: 'Food', date: '02.07.2021', location: 'Konotop', department: 'QI', userName: 'Paul', link: 'www.dannone.com' },
+    { id: 6, vendorName: 'Winchester', promoName: 'Fast bullets ', category: 'Guns', date: '01.07.2021', location: 'London', department: 'FrontEnd', userName: 'Petya', link: 'www.guns.com' },
+    { id: 7, vendorName: 'Faberlic', promoName: 'Chanel #5', category: 'Beauty', date: '12.07.2021', location: 'Kyiv', department: 'HR', userName: 'Jon', link: 'www.faberlic.com' },
+    { id: 8, vendorName: 'Addidas', promoName: 'Boll of Hell', category: 'Sport', date: '18.06.2021', location: 'Minsk', department: 'FrontEnd', userName: 'Bill', link: 'www.addidas.com' },
+    { id: 9, vendorName: 'Roshen', promoName: 'Cockies for 50', category: 'Food', date: '29.07.2021', location: 'Lviv', department: 'BackEnd', userName: 'Smith', link: 'www.roshen.com' },
+    { id: 10, vendorName: 'BMW', promoName: 'Pedals', category: 'Cars', date: '11.07.2021', location: 'Rome', department: 'PM', userName: 'Gretta', link: 'www.bmw.com' },
+    { id: 11, vendorName: 'Danone', promoName: 'No way eat our yogurts', category: 'Food', date: '02.07.2021', location: 'Konotop', department: 'QI', userName: 'Paul', link: 'www.dannone.com' },
+    { id: 12, vendorName: 'Winchester', promoName: 'Fast bullets ', category: 'Guns', date: '01.07.2021', location: 'London', department: 'FrontEnd', userName: 'Petya', link: 'www.guns.com' },
 
 ];
 
--- src/components/admin/statistic/InformationBlock/InformationBlock.tsx
+++ src/components/admin/statistic/InformationBlock/InformationBlock.tsx
@@ -12,13 +12,13 @@ const InformationBlock = () => {
                 <InfoItem title="Most active location:" value="London" />
             </div>
             <div className="InfoBlock">
-                <InfoItem title="Number of Brands:" value="54" />
+                <InfoItem title="Number of Vendors:" value="54" />
                 <InfoItem title="Number of promos:" value="213" />
                 <InfoItem title="Number of active promos:" value="39" />
                 <InfoItem title="Promotions durations:" value="17 days" />
             </div>
             <div className="InfoBlock">
-                <InfoItem title="The most popular Brand:" value="Cassio" />
+                <InfoItem title="The most popular Vendor:" value="Cassio" />
                 <InfoItem title="The most popular Promo:" value="Hail LITRA!!" />
                 <InfoItem title="The most popular category:" value="Sport" />
                 <InfoItem title="The most popular subcategory:" value="Shoes" />
