453dbf8c0e8952e208744a7d33643ca9c97a0d1f
labchuk <labchuks70@gmail.com>
2021-07-01 12:23:11 +0300

    fix chip

--- src/components/common/SearchBar/SelectMultiple/Сategory.tsx
+++ src/components/common/SearchBar/SelectMultiple/Сategory.tsx
@@ -1,20 +1,32 @@
 import React,{useState}from "react"
 import {SelectMultiple} from "../../../index"
+import {useAppDispatch, useAppSelector} from "../../../../store/Redux-toolkit-hook";
 
-const Сategory = ({clName,data,name}:{clName:string, data:string[],name:string}) => {
+const Сategory = ({clName,data,name}:{clName:string, data: object,name:string}) => {
     
     const [arrTag, setArrTag] = useState<string[]>([])
+    const chipData = useAppSelector(state => state.chips.ChipsArray)
+    const dispatch = useAppDispatch();
 
     const addSetArrTag = (tag:any) =>{
         setArrTag(tag);
     }
+    const filterChips = ():any => {
+        const list = []
+        for (const i in chipData) {
+            if (chipData[i].id.slice(0,4) === name.slice(0,4)){
+                list.push(chipData[i].label)
+            }
+        }
+        return list
+    }
 
     return(
         <>
-        <SelectMultiple data={data} clName={clName} name={name} setArrTag={addSetArrTag}/>
-        {arrTag.map(item=>
-            <SelectMultiple data={data} clName={clName} name={item} setArrTag={null}/>
-        )}
+        <SelectMultiple data={data} clName={clName}  name={name} />
+        { filterChips().length>0? filterChips().map((item: string)=>
+            <SelectMultiple data={data} clName={clName} name={item}/>
+        ): null}
         </>
     );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -33,7 +33,7 @@ export { default as GraphInfo } from "./admin/statistic/GraphInfo/GraphInfo";
 export { default as Graph } from "./admin/statistic/Graph/Graph";
 
 export { default as SelectMultiple} from "./common/SearchBar/SelectMultiple/SelectMultiple";
-export { default as Сategory} from "./common/SearchBar/SelectMultiple/Сategory";
+export { default as Category} from "./common/SearchBar/SelectMultiple/Сategory";
 export { default as MySelect } from "./common/SearchBar/Select/MySelect";
 export { default as ControlLabel } from "./common/SearchBar/ControlLable/ControlLabel";
 export { default as RatingStar } from "./common/SearchBar/Rating/RatingStar/RatingStar"
