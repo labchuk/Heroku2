be35829ac7864eec398093ef24932b9eb41bf14a
labchuk <labchuks70@gmail.com>
2021-07-19 20:50:52 +0300

    deploy

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const cors = require('cors')
 const PORT = process.env.PORT || 8000
 
 const app = express()
-app.use(cors())
+const corsOptions ={
+    origin: process.env.PORT,
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions))
 app.use(express.static(__dirname))
 app.use(express.static(path.resolve(__dirname, 'build')))
 
