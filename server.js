7f99efdcad674525a2612569df6a1a4e90bb81d0
labchuk <labchuks70@gmail.com>
2021-07-20 15:22:33 +0300

    deploy

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const path = require('path')
+const cors = require('cors')
+
 
 
 const PORT = process.env.PORT || 8000
 
 const app = express()
 
+app.use(cors())
 app.use(express.static(__dirname))
 app.use(express.static(path.resolve(__dirname, 'build')))
+app.use(express.json())
 
 app.get('*', (req,res)=> {
     res.sendFile(path.join(__dirname,"build", 'index.html'))
