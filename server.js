f5a52207594c61e68d144a741b9821e77fc7a130
labchuk <labchuks70@gmail.com>
2021-07-20 16:21:01 +0300

    deploy

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const cors = require('cors')
-const favicon = require('express-favicon');
+
 
 
 
@@ -11,7 +11,6 @@ const app = express()
 
 app.use(cors())
 app.use(express.static(__dirname))
-app.use(favicon(__dirname + '/build/favicon.ico'));
 app.use(express.static(path.join(__dirname, 'build')))
 app.use(express.json())
 
