a0cf5325ecf636c67a7aa51c07c00c8c7e606655
labchuk <labchuks70@gmail.com>
2021-07-26 23:42:43 +0300

    deploy

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static(__dirname))
 app.use(express.static(path.join(__dirname, 'build')))
 app.use(express.json())
 
-app.get('/', (req,res)=> {
+app.get('*', (req,res)=> {
     res.sendFile(path.join(__dirname,"build", 'index.html'))
     console.log("start")
 })
